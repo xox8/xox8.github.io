b7ded9c56becc7cb2d114994882654f122746f7c
xox8 <56340138+xox8@users.noreply.github.com>
2019-10-09 17:19:40 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox-sw.js");
-var cacheStorageKey = 'minimal-pwa-3'
+var cacheStorageKey = 'minimal-pwa-1'
 var cacheList=[
   '/',
   'index.html',
   'main.css',
-  'youhun.jpg'
+  'youhun.png'
 ]
 self.addEventListener('install',e =>{
   e.waitUntil(
