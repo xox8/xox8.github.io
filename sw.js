25eb24f2ddad45f9625e5961aa12f43a2239386a
xox8 <56340138+xox8@users.noreply.github.com>
2019-10-09 18:30:01 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,57 +1,15 @@
-importScripts('https://cdn.jsdelivr.net/npm/workbox-sw@4.3.1/build/workbox-sw.min.js');
-
-if (workbox) {
-    console.log("Yay! Workbox is loaded 🎉");
-} else {
-    console.log("Boo! Workbox didn't load 😬");
-}
-
-var cacheFiles = [
-    {
-        url:
-        '/',
-        'index.html',
-        'main.css',
-        'youhun.png'
-        revision: 'v2' // 加revision，版本改了以後，sw.js 在 application 上會更新
-    }
-];
-workbox.precaching.precacheAndRoute(cacheFiles);
-
-workbox.routing.registerRoute(
-    'https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css',
-    new workbox.strategies.StaleWhileRevalidate()
-);
-workbox.routing.registerRoute(
-    'https://cdn.jsdelivr.net/npm/jquery@3.4.1/dist/jquery.min.js',
-    new workbox.strategies.StaleWhileRevalidate()
-);
-workbox.routing.registerRoute(
-    'https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.bundle.min.js',
-    new workbox.strategies.StaleWhileRevalidate()
-);
-
-//缓存文件
-workbox.routing.registerRoute(
-    /\.css$/,   //通过正则匹配需要缓存哪些文件
-    new workbox.strategies.StaleWhileRevalidate({
-        cacheName: 'css-cache-v1'  //缓存名，可在application-> Cache storage下找到
-    })
-);
-
-workbox.routing.registerRoute(
-    /\.(?:js)$/,
-    new workbox.strategies.StaleWhileRevalidate({
-        cacheName: 'js-cache-v1'
-    })
-);
-
-workbox.routing.registerRoute(
-    // Cache image files.
-    /\.(?:png|jpg|jpeg|svg|gif)$/,
-    // Use the cache if it's available.
-    new workbox.strategies.CacheFirst({
-        // Use a custom cache name.
-        cacheName: 'image-cache-v1'
-    })
-);
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox-sw.js");
+var cacheStorageKey = 'minimal-pwa-1'
+var cacheList=[
+  '/',
+  'index.html',
+  'main.css',
+  'youhun.png'
+]
+self.addEventListener('install',e =>{
+  e.waitUntil(
+    caches.open(cacheStorageKey)
+    .then(cache => cache.addAll(cacheList))
+    .then(() => self.skipWaiting())
+  )
+})
