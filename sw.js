1f1ccbe51aa83d66ebf766fbe581c688d51a7f81
xox8 <56340138+xox8@users.noreply.github.com>
2019-11-26 15:38:27 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.1.0/workbox-sw.js");
-var cacheStorageKey = 'minimal-pwa-10282352'
+var cacheStorageKey = 'minimal-pwa-11261538'
 var cacheList=[
   '/',
   'index.html',
